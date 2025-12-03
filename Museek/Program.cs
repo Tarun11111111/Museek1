3e7c50d1c3798cd3c9739deb0538b036f995152e
ZIN HMUE PAING 2403809C <2403809C@student.tp.edu.sg>
2025-12-03 23:21:09 +0800

    Deleted Unnecessary Pages (Counter.razor and Weather.razor)

--- Museek/Program.cs
+++ Museek/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<MuseekUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<MuseekContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
